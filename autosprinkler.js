998a41981a9614afd3ac27ff1549f0eb8563a23c
official-techsupport <official_techsupport@protonmail.com>
2025-12-24 03:59:11 +0200

    eols + use let/const

--- autosprinkler.js
+++ autosprinkler.js
@@ -12,20 +12,20 @@
 (function() {
     'use strict';
 
-    var wasWaterable = false; // avoid spam when actually unwaterable
+    let wasWaterable = false; // avoid spam when actually unwaterable
 
     function f() {
-        let delay = 5000 + Math.floor(Math.random() * 100);
+        const delay = 5000 + Math.floor(Math.random() * 100);
         setTimeout(f, delay);
-        let droplets = document.querySelectorAll('button > div > div > img.emoji[data-name=\uD83D\uDCA7]');
+        const droplets = document.querySelectorAll('button > div > div > img.emoji[data-name=\uD83D\uDCA7]');
         if (!droplets.length) {
             console.log('No droplets');
             return;
         }
-        let button = droplets[droplets.length - 1].parentElement.parentElement.parentElement;
-        let comment = button.parentElement.parentElement.parentElement.parentElement;
-        let text = comment.innerText;
-        let canWater = text.includes('\nReady to be watered!\n');
+        const button = droplets[droplets.length - 1].parentElement.parentElement.parentElement;
+        const comment = button.parentElement.parentElement.parentElement.parentElement;
+        const text = comment.innerText;
+        const canWater = text.includes('\nReady to be watered!\n');
         console.log(JSON.stringify({wasWaterable, canWater}));
         if (canWater && !wasWaterable) {
             console.log('Watering!');
